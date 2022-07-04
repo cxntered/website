32d9fffcf79aa8c99208ecfba93a512799329e1d
cxntered <67499489+cxntered@users.noreply.github.com>
2022-07-05 00:23:00 +0800

    change space to tab + added technoblade crown adder to projects

--- js/navbar.js
+++ js/navbar.js
@@ -1,23 +1,23 @@
 const navbar = document.getElementById("navbar");
 const navbarToggle = navbar.querySelector(".navbar-toggle");
-function openMobileNavbar () {
-  navbar.classList.add("opened");
-  navbarToggle.setAttribute("aria-label", "Close navigation menu.");
+function openMobileNavbar() {
+	navbar.classList.add("opened");
+	navbarToggle.setAttribute("aria-label", "Close navigation menu.");
 }
-function closeMobileNavbar () {
-  navbar.classList.remove("opened");
-  navbarToggle.setAttribute("aria-label", "Open navigation menu.");
+function closeMobileNavbar() {
+	navbar.classList.remove("opened");
+	navbarToggle.setAttribute("aria-label", "Open navigation menu.");
 }
 navbarToggle.addEventListener("click", () => {
-  if (navbar.classList.contains("opened")) {
-    closeMobileNavbar();
-  } else {
-    openMobileNavbar();
-  }
+	if (navbar.classList.contains("opened")) {
+		closeMobileNavbar();
+	} else {
+		openMobileNavbar();
+	}
 });
 const navbarMenu = navbar.querySelector(".navbar-menu");
 const navbarLinksContainer = navbar.querySelector(".navbar-links");
 navbarLinksContainer.addEventListener("click", (clickEvent) => {
-  clickEvent.stopPropagation();
+	clickEvent.stopPropagation();
 });
 navbarMenu.addEventListener("click", closeMobileNavbar);
