0b0dbf64e0bb2cc1a639fe199755c49f725629f7
cxntered <cxntered@duck.com>
2023-07-31 05:47:01 +1000

    📦 NEW: add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,25 @@
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({ weight: "900", subsets: ["latin"] });
+
+export default function NotFound() {
+	return (
+		<main className="flex min-h-screen flex-row items-center justify-center px-12 py-20">
+			<div className="sm:flex">
+				<h1
+					className={`${montserrat.className} text-3xl sm:text-4xl md:text-5xl font-extrabold`}
+				>
+					404
+				</h1>
+				<div className="sm:border-l sm:border-gray-200 sm:pl-6 sm:ml-6">
+					<h1 className="text-3xl sm:text-4xl md:text-5xl font-semibold">
+						Page not found
+					</h1>
+					<p className="mt-3 text-gray">
+						Please check the URL in the address bar and try again.
+					</p>
+				</div>
+			</div>
+		</main>
+	);
+}
