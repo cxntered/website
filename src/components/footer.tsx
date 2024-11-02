1245ad28a5119b68335a9359abcb01aa2468a0b7
cxntered <cxntered@duck.com>
2024-11-02 23:37:38 +1100

    ✨ feature: update footer update copyright date and add a blurred background

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
 	return (
 		<div className="fixed bottom-0 inset-x-0 z-50 p-4 text-center">
-			<p className="text-sm text-footer">© 2023 cxntered</p>
+			<div className="inline-block mx-auto">
+				<p className="text-sm text-footer backdrop-blur-lg p-2 rounded-full">© 2024 cxntered</p>
+			</div>
 		</div>
 	);
 }
